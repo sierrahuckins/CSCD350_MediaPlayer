1ce59aef130ee4b8cfce4f043556fc2c9762dfa4
bemehiser <bemehiser@gmail.com>
2015-11-29 22:09:38 -0800

    Added basic OnItemLongClick Dialog Box functionality to ResultListActivity

--- app/src/main/java/com/mediaplayer/tba/cscd350_mediaplayer/ResultListsActivity.java
+++ app/src/main/java/com/mediaplayer/tba/cscd350_mediaplayer/ResultListsActivity.java
@@ -1,15 +1,23 @@
 package com.mediaplayer.tba.cscd350_mediaplayer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,15 +25,15 @@ import java.util.List;
 
 
 /**
-*ResultsListsActivity.java
-*Author: Sierra Huckins
-*Revision: 1
-*Date: 20151113
-*Rev. Author: Sierra Huckins
-*Description: Activity that allows user to display database contents based on 1 of 5 factors: artists, albums, playlists, genres, and songs.
-*Results from user's choice within the displayed contents are returned to the MainActivity for playing.
-**/
-public class ResultListsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+ *ResultsListsActivity.java
+ *Author: Sierra Huckins
+ *Revision: 1
+ *Date: 20151113
+ *Rev. Author: Sierra Huckins
+ *Description: Activity that allows user to display database contents based on 1 of 5 factors: artists, albums, playlists, genres, and songs.
+ *Results from user's choice within the displayed contents are returned to the MainActivity for playing.
+ **/
+public class ResultListsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     //member variables for buttons and clickable display
     private Button artistsBtn;
@@ -95,6 +103,48 @@ public class ResultListsActivity extends AppCompatActivity implements View.OnCli
         adapter = new ArrayAdapter<>(this, R.layout.list_item, R.id.list_entry, currentList);
         // set the adapter on the list in content_resultslistactivity
         listResults.setAdapter(adapter);
+
+        // register as a long click listener. we will show a dialog box
+        listResults.setOnItemLongClickListener(this);
+
+        // register as context menu listener
+//        registerForContextMenu(listResults);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // get adapter context menu information
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        // get the index of item clicked
+        int menuItemIndex = item.getItemId();
+        // get the menu items
+        String[] menuItems = getResources().getStringArray(R.array.menu);
+        String menuItemName = menuItems[menuItemIndex];
+        // get the entry in the list view which was clicked
+        String listItemName = currentList.get(info.position);
+
+        Toast.makeText(ResultListsActivity.this, "Item Clicked: " + menuItemName + " current list item: " + listItemName, Toast.LENGTH_SHORT).show();
+
+        // return true, we handled the click
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        // if the created context menu is on the list results view
+        if (v.getId()==R.id.listResults) {
+            // get adapter view context menu information
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+            // set menu header to be the current position title
+            menu.setHeaderTitle(currentList.get(info.position));
+            // menu items as defined by resource array
+            String[] menuItems = getResources().getStringArray(R.array.menu);
+            // add items to menu
+            for (int i = 0; i < menuItems.length; i++) {
+                menu.add(Menu.NONE, i, i, menuItems[i]);
+            }
+        }
     }
 
     //onClick listener for buttons
@@ -197,7 +247,63 @@ public class ResultListsActivity extends AppCompatActivity implements View.OnCli
         }
 
         // update current list
+    }
 
+    /**
+     * Callback method to be invoked when an item in this view has been
+     * clicked and held.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need to access
+     * the data associated with the selected item.
+     *
+     * @param parent   The AbsListView where the click happened
+     * @param view     The view within the AbsListView that was clicked
+     * @param position The position of the view in the list
+     * @param id       The row id of the item that was clicked
+     * @return true if the callback consumed the long click, false otherwise
+     */
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // TODO: 11/29/2015 instead of setItems, use setAdapter(ListAdapter) to set a dynamic list of all playlists, or option to add new playlist
+
+        // build a new dialog box for choosing which playlist to add the item(s) to
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Dialog Message")
+                .setTitle("Select Playlist")
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(ResultListsActivity.this, "Add was clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("New", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ResultListsActivity.this, "Adding new Playlist", Toast.LENGTH_SHORT).show();
+                        // TODO: 11/29/2015 make this prompt for playlist title, and add playlist
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ResultListsActivity.this, "Cancel was clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setItems(R.array.menu, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ResultListsActivity.this, "Item " + which + " was clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // build dialog
+        AlertDialog dialog = builder.create();
+
+        // show dialog
+        dialog.show();
+
+        // return we handled the event
+        return true;
     }
 
     private void returnIntent(MediaFile[] mediaFiles) {
